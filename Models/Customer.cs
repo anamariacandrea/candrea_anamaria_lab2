62e2ff0528b3615536e231cdb525af12c735ec5f
Ana-Maria Candrea <candrea_anamaria@yahoo.com>
2020-10-31 21:42:36 +0200

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace candrea_anamaria_lab2.Models
+{
+    public class Customer
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public DateTime BirthDate { get; set; }
+//navigation property- un customer poate fi relationat cu mai multe entitati Orders
+        public ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace candrea_anamaria_lab2.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public int CustomerID { get; set; }
+        public int BookID { get; set; }
+        public Customer Customer { get; set; }
+        public Book Book { get; set; }
+
+    }
+}
