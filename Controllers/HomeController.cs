22f6ea52af6d709ec82dc55180f5ffb4cc1d23ad
Ana-Maria Candrea <candrea_anamaria@yahoo.com>
2020-11-01 19:51:45 +0200

    Laborator 4 pana la eroare

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,11 +6,32 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using candrea_anamaria_lab2.Models;
+using Microsoft.EntityFrameworkCore;
+using candrea_anamaria_lab2.Data;
+using candrea_anamaria_lab2.Models.LibraryViewModels;
+using SQLitePCL;
 
 namespace candrea_anamaria_lab2.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly LibraryContext _context;
+        public HomeController(LibraryContext context)
+        {
+            _context = context;
+        }
+        public async Task<ActionResult> Statistics()
+        {
+            IQueryable<OrderGroup> data = from order in _context.Orders
+                                          group order by order.OrderDate into dateGroup
+                                          select new OrderGroup()
+                                          {
+                                              OrderDate = dateGroup.Key,
+                                              BookCount = dateGroup.Count()
+                                          };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
